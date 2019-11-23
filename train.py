a5b550ffc45e64318436259b51ac5504774078d5
TTrapper <mike.sk.traynor@gmail.com>
2019-11-23 14:53:19 -0400

    option to restore training from saved model

--- train.py
+++ train.py
@@ -1,17 +1,30 @@
+import argparse
+
 import numpy as np
 import tensorflow as tf
 
 import data_pipe
 import model_def
 
-def setup():
+parser = argparse.ArgumentParser()
+parser.add_argument('--restore', type=str, choices=['yes', 'no'], default='no')
+
+def parseargs(parser):
+    args = parser.parse_args()
+    args.restore = args.restore == 'yes'
+    return args
+
+def setup(restore):
     learn_rate = 1e-4
     dataset = data_pipe.file_to_dataset()
-    # Get the batch size and sequence length by peeking at the first Dataset element
-    model = model_def.make_model()
-    optimizer = tf.keras.optimizers.Adam(learn_rate)
-    model.compile(optimizer=optimizer,
-                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True))
+    if restore:
+        model = tf.keras.models.load_model('./model.hd5', compile=True,
+            custom_objects={'DNCCell':model_def.DNCCell})
+    else:
+        model = model_def.make_model()
+        optimizer = tf.keras.optimizers.Adam(learn_rate)
+        model.compile(optimizer=optimizer,
+                      loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True))
     model.summary()
     return dataset, model
 
@@ -20,7 +33,6 @@ def train(model, dataset):
     def inference_fn(batch, logs):
         if batch % 2000 == 1999:
             model_def.run_inference(model, 'she', seqlen)
-#            model.save('./saved', overwrite=True, include_optimizer=True)
             model.save('./model.hd5', save_format='h5', overwrite=True, include_optimizer=True)
     inference_callback = tf.keras.callbacks.LambdaCallback(on_batch_end=inference_fn)
     callbacks = [inference_callback]
@@ -28,7 +40,8 @@ def train(model, dataset):
         callbacks=callbacks)
 
 if '__main__' == __name__:
-    dataset, model = setup()
+    args = parseargs(parser)
+    dataset, model = setup(args.restore)
     train(model, dataset)
     _, seqlen = next(iter(dataset))[0].shape
     model_def.run_inference(model, 'hi there', seqlen)
