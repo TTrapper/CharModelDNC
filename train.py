4b17b351d3ad782e674af332d80bba64c4ceb353
TTrapper <mike.sk.traynor@gmail.com>
2019-11-25 14:25:27 -0400

    restore arg uses built in bool flag action

--- train.py
+++ train.py
@@ -7,18 +7,17 @@ import data_pipe
 import model_def
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--restore', type=str, choices=['yes', 'no'], default='no')
+parser.add_argument('--restore', action='store_true')
 
 def parseargs(parser):
     args = parser.parse_args()
-    args.restore = args.restore == 'yes'
     return args
 
 def setup(restore):
     learn_rate = 1e-4
     dataset = data_pipe.file_to_dataset()
     if restore:
-        model = tf.keras.models.load_model('./model.hd5', compile=True,
+        model = tf.keras.models.load_model('./model.h5', compile=True,
             custom_objects={'DNCCell':model_def.DNCCell})
     else:
         batchsize = dataset.element_spec[0].shape[0]
@@ -31,8 +30,8 @@ def setup(restore):
 
 def train(model, dataset):
     def inference_fn(batch, logs):
-        if batch % 2000 == 1999:
-            model.save('./model.hd5', save_format='h5', overwrite=True, include_optimizer=True)
+        if batch % 20000 == 200:
+            model.save('./model.h5', save_format='h5', overwrite=True, include_optimizer=True)
             for softmax_temp in [1e-16, 0.5, 0.75]:
                 model_def.run_inference(model, 'she', 128, softmax_temp)
     inference_callback = tf.keras.callbacks.LambdaCallback(on_batch_end=inference_fn)
