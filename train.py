0ae2a07dfdb8049c9f61ed3f33613369cb85d4b4
TTrapper <mike.sk.traynor@gmail.com>
2020-10-06 12:59:01 -0300

    Do validation by default if file is present

--- train.py
+++ train.py
@@ -11,45 +11,55 @@ import process_config
 parser = argparse.ArgumentParser()
 parser.add_argument('--restore', action='store_true')
 parser.add_argument('--eval', action='store_true')
-parser.add_argument('--data_fp', default='./traindata.txt',
-    help='Path to a text file with a fixed number of bytes per line (generate with prepare_data.py')
+parser.add_argument('--traindata', default='./traindata.txt', help='Path to a text file')
+parser.add_argument('--validdata', default='./validdata.txt', help='Path to a text file')
+parser.add_argument('--evaldata', default='./evaldata.txt', help='Path to a text file')
+
 
 def parseargs(parser):
     args = parser.parse_args()
+    if not args.eval and not os.path.isfile(args.traindata):
+        raise FileNotFoundError('No training data file found at {}'.format(args.traindata))
+    if not args.eval and not os.path.isfile(args.validdata):
+        print('No validation data file found at: {}. Continuing without it.'.format(args.validdata))
+        args.validdata = None
+    if args.eval and not os.path.isfile(args.evaldata):
+        raise FileNotFoundError('No training data file found at {}'.format(args.traindata))
     return args
 
-def setup(restore, data_fp, evalmode):
+def setup(restore, datapath, validdata, evalmode):
     learn_rate = 1e-4
     config = process_config.load_config()
     model = model_def.make_model(config)
-    dataset = data_pipe.file_to_dataset(data_fp, config, maskinputs=not evalmode)
+    dataset = data_pipe.file_to_dataset(datapath, config, maskinputs=not evalmode)
+    valid_dataset = None
+    if validdata:
+        valid_dataset = data_pipe.file_to_dataset(validdata, config, maskinputs=False)
     if restore:
         model.load_weights('./model.h5', by_name=True, skip_mismatch=True)
     optimizer = tf.keras.optimizers.Adam(learn_rate)
     model.compile(optimizer=optimizer,
-        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True))
+            loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True))
     model.summary()
-    return dataset, model
+    return dataset, valid_dataset, model
 
-def train(model, dataset):
-    def inference_fn(batch, logs):
+def train(model, dataset, valid_dataset):
+    def save_fn(batch, logs):
         if batch % 1000 == 200:
-            model.save('./model.h5', save_format='h5', overwrite=True, include_optimizer=True)
-            for softmax_temp in [1e-16, 0.5, 0.75]:
-                context = ' '
-#                model_def.run_inference(model, context, 512, softmax_temp)
-    inference_callback = tf.keras.callbacks.LambdaCallback(on_batch_end=inference_fn)
-    callbacks = [inference_callback]
+            model.save('./model.h5', save_format='h5', overwrite=True, include_optimizer=False)
+    save_callback = tf.keras.callbacks.LambdaCallback(on_batch_end=save_fn)
+    callbacks = [save_callback]
     model.fit(dataset, epochs=200, verbose=1, steps_per_epoch=None, use_multiprocessing=True,
-        callbacks=callbacks)
+            validation_data=valid_dataset, validation_steps=None, callbacks=callbacks)
 
 def evaluate(model, dataset):
     model.evaluate(dataset)
 
 if '__main__' == __name__:
     args = parseargs(parser)
-    dataset, model = setup(args.restore, args.data_fp, args.eval)
+    datapath = args.evaldata if args.eval else args.traindata
+    dataset, valid_dataset, model = setup(args.restore, datapath, args.validdata, args.eval)
     if args.eval:
         evaluate(model, dataset)
     else:
-        train(model, dataset)
+        train(model, dataset, valid_dataset)
